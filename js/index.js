924ac325a7079f9f926ebf0bab865c3b7e635213
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-09-28 19:09:23 +0300

    Language Translation

--- js/index.js
+++ js/index.js
@@ -14,19 +14,26 @@ function loadContent() {
     const lang = localStorage.getItem('lang') || 'ar'; // Default to 'ar' if no language is set
 
     // Define the file path based on the language
-    let filePath = lang === 'ar' ? 'ar.html' : 'en.html';
+    var elements;
+    var sectionsDirection = document.querySelectorAll(".language_directed_section")
+    var direction;
+    if (lang === 'ar'){
+        elements = document.querySelectorAll(".EN")
+        direction = 'rtl'
+    }else{
+        elements = document.querySelectorAll(".AR")
+        direction = 'ltr'
+    }
+    for (var element of elements){
+        element.style.display = "none"
+    }
+    for (var section of sectionsDirection){
+        section.style.direction = direction
+    }
 
-    // Fetch the HTML content of the corresponding file
-    fetch(filePath)
-        .then(response => response.text()) // Get the text content of the HTML file
-        .then(data => {
-            // Insert the fetched HTML content into the #content div
-            document.getElementById('content').innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error loading content:', error);
-        });
+
+    document.getElementById("content").style.display = 'block'
 }
 
 // Call the function to load content when the page loads
-// window.onload = loadContent;
+window.onload = loadContent;
